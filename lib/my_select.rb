fb39505f891ab9bedda0b27bfdc251f6eb1a625d
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-14 15:41:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,19 +2,14 @@ def my_select(collection)
  i = 0
   new_array = []
   while i < collection.length 
-   new_array << yield()
-  yield(collection[i])
-  i = i + 1
+    new_array << yield(collection[i])
+    i = i + 1
   end
-  
   collection 
 end
 
 
-  while i < array.length
-   
-    i += 1
-  end
+  
   new_array
 
 
