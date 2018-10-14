181fa61f0771f1033df833accfdce7f3811c59ab
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-14 15:39:00 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,5 @@
 def my_select(collection)
- 
-end
-
-def my_each(collection) # put argument(s) 
-  i = 0
+ i = 0
   while i < collection.length 
   yield(collection[i])
   i = i + 1
@@ -13,14 +9,16 @@ def my_each(collection) # put argument(s)
 end
 
 
-The .select method returns a new collection containing all of the elements in the submitted collection for which the block's conditional is true:
 
-cool_nums = [1, 2, 3, 4, 5]
+
+#The .select method returns a new collection containing all of the elements in the submitted collection for which the block's conditional is true:
+
+#cool_nums = [1, 2, 3, 4, 5]
  
-def even_nums(nums)
-  nums.select do |x|
-    x.even?
-  end
-end
+#def even_nums(nums)
+ # nums.select do |x|
+  #  x.even?
+ # end
+#end
  
-even_nums(cool_nums)
+#even_nums(cool_nums)
