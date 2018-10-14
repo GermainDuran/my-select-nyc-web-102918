8b20029badca22e6e8d6450f287ca53c43ef9f49
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-14 15:37:10 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  
 end
+
+
+
+The .select method returns a new collection containing all of the elements in the submitted collection for which the block's conditional is true:
+
+cool_nums = [1, 2, 3, 4, 5]
+ 
+def even_nums(nums)
+  nums.select do |x|
+    x.even?
+  end
+end
+ 
+even_nums(cool_nums)
